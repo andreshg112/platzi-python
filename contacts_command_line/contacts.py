78afeb1730ed1671733785e038b097c801024b3e
Andrés Herrera García <andreshg112@gmail.com>
2019-10-12 00:15:35 -0500

    Persistencia de datos

--- contacts_command_line/contacts.py
+++ contacts_command_line/contacts.py
@@ -1,4 +1,5 @@
 #
+import csv
 
 
 class Contact:
@@ -17,6 +18,7 @@ class ContactBook:
     def add(self, name, phone, email):
         contact = Contact(name, phone, email)
         self._contacts.append(contact)
+        self._save()
 
     def show_all(self):
         for contact in self._contacts:
@@ -26,6 +28,7 @@ class ContactBook:
         for idx, contact in enumerate(self._contacts):
             if contact.name.lower() == name.lower():
                 del self._contacts[idx]
+                self._save()
                 break
 
     def search(self, name):
@@ -35,6 +38,14 @@ class ContactBook:
                 break
         else:
             self._not_found()
+
+    def _save(self):
+        with open('contacts.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(('name', 'phone', 'email'))
+
+            for contact in self._contacts:
+                writer.writerow((contact.name, contact.phone, contact.email))
 
     def _print_contact(self, contact):
         print('--- * --- * --- * --- * --- * --- * --- * ---')
@@ -52,6 +63,14 @@ class ContactBook:
 def run():
 
     contact_book = ContactBook()
+
+    with open('contacts.csv', 'r') as f:
+        reader = csv.reader(f)
+        for idx, row in enumerate(reader):
+            if idx == 0:
+                continue
+
+            contact_book.add(row[0], row[1], row[2])
 
     while True:
         command = str(input('''
