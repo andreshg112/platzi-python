e7b5f08ec593aac9456df3f890688422fe0a09be
Andrés Herrera García <andreshg112@gmail.com>
2019-10-11 23:59:53 -0500

    Lógica de directorio de contactos

--- contacts_command_line/contacts.py
+++ contacts_command_line/contacts.py
@@ -3,10 +3,10 @@
 
 class Contact:
 
-    def __init(self, name, phone, email):
-        self._name = name
-        self._phone = phone
-        self._email = email
+    def __init__(self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
 
 
 class ContactBook:
@@ -15,7 +15,38 @@ class ContactBook:
         self._contacts = []
 
     def add(self, name, phone, email):
-        print('name: {}, phone: {}, email: {}'.format(name, phone, email))
+        contact = Contact(name, phone, email)
+        self._contacts.append(contact)
+
+    def show_all(self):
+        for contact in self._contacts:
+            self._print_contact(contact)
+
+    def delete(self, name):
+        for idx, contact in enumerate(self._contacts):
+            if contact.name.lower() == name.lower():
+                del self._contacts[idx]
+                break
+
+    def search(self, name):
+        for contact in self._contacts:
+            if contact.name.lower() == name:
+                self._print_contact(contact)
+                break
+        else:
+            self._not_found()
+
+    def _print_contact(self, contact):
+        print('--- * --- * --- * --- * --- * --- * --- * ---')
+        print('Nombre: {}'.format(contact.name))
+        print('Teléfono: {}'.format(contact.phone))
+        print('Email: {}'.format(contact.email))
+        print('--- * --- * --- * --- * --- * --- * --- * ---')
+
+    def _not_found(self):
+        print('*******')
+        print('¡No encontrado!')
+        print('*******')
 
 
 def run():
@@ -45,13 +76,19 @@ def run():
             print('actualizar contacto')
 
         elif command == 'b':
-            print('buscar contacto')
+
+            name = str(input('Escribe el nombre del contacto: '))
+
+            contact_book.search(name)
 
         elif command == 'e':
-            print('eliminar contacto')
+            name = str(input('Escribe el nombre del contacto: '))
+
+            contact_book.delete(name)
 
         elif command == 'l':
-            print('listar contacto')
+
+            contact_book.show_all()
 
         elif command == 's':
             break
