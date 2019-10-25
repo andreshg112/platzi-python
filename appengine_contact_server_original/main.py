d36925e9768c0c1049c04b0933a2e2177b20fdd8
Andrés Herrera García <andreshg112@gmail.com>
2019-10-25 00:16:21 -0500

    Listar y eliminar contactos

--- appengine_contact_server_original/main.py
+++ appengine_contact_server_original/main.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 from contact_model import Contact
 app = Flask(__name__)
+app.secret_key = 'some_secret'
+app.debug = True
 
 
 @app.route(r'/', methods=['GET'])
 def contact_book():
-    return render_template('contact_book.html')
+    contacts = Contact.query().fetch()
+    return render_template('contact_book.html', contacts=contacts)
 
 
 @app.route(r'/add', methods=['GET', 'POST'])
@@ -19,9 +22,26 @@ def add_contact():
                           email=request.form.get('email'))
 
         contact.put()
+        flash('¡Se añadió el contacto!')
 
     return render_template('add_contact.html')
 
+
+@app.route(r'/contacts/<uid>', methods=['GET'])
+def contact_details(uid):
+    contact = Contact.get_by_id(int(uid))
+
+    if not contact:
+        return redirect('/', code=301)
+
+    return render_template('contact.html', contact=contact)
+
+
+@app.route(r'/delete', methods=["POST"])
+def delete_contact():
+    contact = Contact.get_by_id(request.form.get('uid'))
+    contact.key.delete()
+    return redirect('/contacts/{}'.format(contact.key.id()))
 
 
 if __name__ == '__main__':
